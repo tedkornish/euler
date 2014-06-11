9fdc5cbc7f49e03e019ae1034ec93dddc6cbb1db
Ted Kornish <ted.kornish+github@gmail.com>
2014-06-11 12:53:14 -0400

    Add comments to 22 to help remember component functions

--- 022.py
+++ 022.py
@@ -1,7 +1,13 @@
+# letters and values - { 'A': 1, 'B': 2, ... , 'Z': 26 }
 vals = dict(zip(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'), range(1, 26 + 1)))
+
+# score('HELLO') -> ['H', 'E', 'L', 'L', 'O']
+# -> [8, 5, 12, 12, 15] -> 8 + 5 + 12 + 12 + 15 -> 52
 score = lambda name: sum(map(lambda x: vals[x], list(name)))
-positional_scorer = lambda x: (x[0] + 1) * score(x[1])
+
+# positional_score((3, 'ABC')) -> 4 * (1 + 2 + 3) -> 24
+positional_score = lambda x: (x[0] + 1) * score(x[1])
 
 with open('names.txt', 'r') as f:
   names = sorted(f.read().replace('"', "").split(','))
-  print sum(map(positional_scorer, list(enumerate(names))))
+  print sum(map(positional_score, list(enumerate(names))))
